74bcd38728b28f738d6848004efa44070d01eed7
Saumil Kalavadia <kalavadiasaumil35@gmail.com>
2025-08-29 21:04:57 +0530

    factorialcalculator.py
    Added While True statement and try-except one for enhanced usability

--- factorialcalculator.py
+++ factorialcalculator.py
@@ -1,10 +1,24 @@
-def fac(n):#defining a function 
+while True:
+	
+	def fac(n):#defining a function 
+		try:
 	
 	
-	if(n==0 or n==1):
-		return 1					 
-	else:
-		b=n*fac(n-1)       #logic setting , if n is 3 then 3 * fac(2) which is 2*fac(1) hence fac 1 is 1 then the interpreter will eventually do 3*2*1   .
-		return(b)
-n=int(input("Enter the number whose factorial you want: "))
-print(fac(n))
+			if(n==0 or n==1):
+				return 1					#logic setting , if n is 3 then 3 * fac(2) which is 2*fac(1) hence fac 1 is 1 then the interpreter will eventually do 3*2*1   . 
+			else:
+				b=n*fac(n-1)
+				return(b)
+		except:
+			print("Invalid input!!!!")
+	
+	try:
+		
+		n=int(input("Enter the number whose factorial you want: "))
+		print(fac(n))
+	except:
+		print("Invalid input!!")
+	
+	q=input("Do you want to continue?(y/n): ").lower()
+	if q=="n":
+		break
